600ae6d5672b3a842452e60e6d6b3e4f5d84b9b6
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 01:21:32 -0700

    Create SetObject interface

---
+++ src/shared/interfaces.ts
@@ -0,0 +1,12 @@
+
+export interface SetObject {
+    x: number,
+    y: number,
+    z: number,
+    xRot: number,
+    yRot: number,
+    zRot: number,
+    var1: number,
+    var2: number,
+    var3: number,
+}
