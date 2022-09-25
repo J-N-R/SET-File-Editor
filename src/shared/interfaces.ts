4bf6ae11c6185cfbfedc5e4ce0dcf06f93522514
BuildTools <unconfigured@null.spigotmc.org>
2022-09-25 00:35:27 -0700

    Create mock objects for frontend development

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -1,12 +1,14 @@
-
+/** Typescript representation of a SET object. */
 export interface SetObject {
+    type: string,
     x: number,
     y: number,
     z: number,
+    // Rotation data stored as degrees, converted to BAMS later.
     xRot: number,
     yRot: number,
     zRot: number,
-    var1: number,
-    var2: number,
-    var3: number,
+    var1?: number,
+    var2?: number,
+    var3?: number,
 }
---
+++ src/shared/mock-objects.ts
@@ -0,0 +1,32 @@
+import { SetObject } from './interfaces';
+
+/** Mock Objects used for initial frontend development. */
+export const OBJECTS: SetObject[] = [
+    {
+        type: 'Spring',
+        x: 0,
+        y: 0,
+        z: 0,
+        xRot: 0,
+        yRot: 0,
+        zRot: 0,
+    },
+    {
+        type: 'Beetle',
+        x: 0,
+        y: 0,
+        z: 0,
+        xRot: 0,
+        yRot: 0,
+        zRot: 0,
+    },
+    {
+        type: 'Dash Board',
+        x: 10,
+        y: 0,
+        z: 20,
+        xRot: 0,
+        yRot: 90,
+        zRot: 0,
+    },
+];
