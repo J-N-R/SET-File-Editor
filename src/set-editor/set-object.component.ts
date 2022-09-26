174b0541f6ea2f188b11e7e438b3aa75cbf5806c
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 01:31:59 -0700

    Plug in SetObject functionality to SetObjectComponent, minor css fixes

--- src/set-editor/set-object.component.ts
+++ src/set-editor/set-object.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SetObject } from '../shared/interfaces';
 
 @Component({
   selector: 'app-set-object',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-object.component.scss']
 })
 export class SetObjectComponent implements OnInit {
+  @Input() object: SetObject = {
+    oid: 0,
+    name: '',
+    x: 0,
+    y: 0,
+    z: 0,
+    xRot: 0,
+    yRot: 0,
+    zRot: 0,
+  };
 
   constructor() { }
 
