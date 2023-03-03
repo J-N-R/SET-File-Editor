0735ad983bae95038cdcfdc53346f9741a86819d
Jonathan Rivera <rivejona@kean.edu>
2023-03-03 02:15:43 -0800

    Reset mocked objects

--- src/shared/mock-objects.ts
+++ src/shared/mock-objects.ts
@@ -1,9 +1,8 @@
 import { SetObject } from './interfaces';
-import { SA2Object } from './objects';
 import { CITY_ESCAPE_OBJECTS } from './sa2-levels';
 
 /** Mock Objects used for initial frontend development. */
-const NUMBER_OF_OBJECTS = 300;
+const NUMBER_OF_OBJECTS = 1;
 const OBJECTS = Array.from(CITY_ESCAPE_OBJECTS.values());
 
 export const MOCK_OBJECTS: SetObject[] = [...Array(NUMBER_OF_OBJECTS).keys()]
