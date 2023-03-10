ba2135dafb264548f31885b3dc34c9ef4fd5a1c6
Jonathan Rivera <rivejona@kean.edu>
2023-03-09 22:26:51 -0800

    Update version to 1.02

--- src/shared/content.ts
+++ src/shared/content.ts
@@ -1,7 +1,7 @@
 import { SetObject, SetLabel, SortingOption, SetFile } from './interfaces';
 
 /** Version of the Set Editor. */
-export const VERSION = '1.01';
+export const VERSION = '1.02';
 
 /** Keys used to iterate over an object's custom properties. */
 export const CUSTOM_KEYS: (keyof SetLabel)[] = ['var1', 'var2', 'var3'];
