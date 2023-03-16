f58079d5baf86d8db1930f9bf3100d07f8d556d6
Jonathan Rivera <rivejona@kean.edu>
2023-03-16 00:51:56 -0700

    Add tests to Set Editor component

--- src/set-editor/set-editor.component.spec.ts
+++ src/set-editor/set-editor.component.spec.ts
@@ -1,28 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { of as observableOf } from 'rxjs';
 
 import SetEditorComponent from './set-editor.component';
+import { ElectronService } from '../shared/electron.service';
 
 describe('SetEditorComponent', () => {
   let component: SetEditorComponent;
   let fixture: ComponentFixture<SetEditorComponent>;
+  let router: Router;
+  let electronService: ElectronService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
+      providers: [ElectronService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(SetEditorComponent);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
+    electronService = TestBed.inject(ElectronService);
     fixture.detectChanges();
   });
 
-  /** TODO: Test that query parameters work properly. */
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('has a loading state', () => {
+    const loadingEl = fixture.nativeElement.querySelector('mat-spinner');
+    expect(loadingEl).toBeDefined();
+  });
+
+  it('reads from query parameters', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue(getTestUrl());
+    component.ngOnInit();
+
+    expect(component.isSA2Format).toEqual(true);
+    expect(component.fileName).toEqual('testFileName.bin');
+    expect(component.fileType).toEqual('S');
+    expect(component.stage).toEqual(13);
+  });
+
+  it('attempts to read from file if path is detected', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue(getTestUrl(true));
+    spyOn(electronService, 'readFile').and.returnValue(observableOf([]));
+    component.ngOnInit();
+
+    const mockSetFile = {
+      fileName: 'testFileName.bin',
+      isSA2Format: true,
+      stage: 13,
+      setObjects: [],
+      filePath: 'testPath',
+    }
+    expect(electronService.readFile).toHaveBeenCalledWith(mockSetFile);
   });
 });
+
+function getTestUrl(hasFilePath = false): string {
+  return [
+    '/',
+    '?isSA2Format=true',
+    '&fileName=testFileName',
+    '&fileType=S',
+    '&stage=13',
+    hasFilePath ? '&filePath=testPath' : [],
+  ].flat().join('');
+}
 
 
 
