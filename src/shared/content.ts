fa40bcdc3e6946663836e7d6c3aaae898dca1a50
Jonathan Rivera <rivejona@kean.edu>
2023-04-11 18:13:18 -0700

    Update vresion number

--- src/shared/content.ts
+++ src/shared/content.ts
@@ -1,7 +1,7 @@
 import { SetObject, SetLabel, SortingOption, SetFile } from './interfaces';
 
 /** Version of the Set Editor. */
-export const VERSION = '1.22';
+export const VERSION = '2.00';
 
 /** URL to find latest Set Editor version. */
 export const VERSION_URL = 'https://raw.githubusercontent.com/J-N-R/' +
